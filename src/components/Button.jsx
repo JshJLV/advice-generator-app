719097b00470cf624508e4dec14af4965ce21e85
Joseph Lopez <joseph.lpz1999@gmail.com>
2023-07-07 12:40:36 -0600

    add styles file

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,16 @@
 import iconDice from "../assets/icon-dice.svg";
+import "./Button.css";
 
 function Button({ handleButton }) {
   return (
     <button
       onClick={handleButton}
-      className="bg-bg-neon-green p-4 rounded-full hover:shadow-bg-neon-green "
+      className="
+      bg-bg-neon-green 
+      p-4 
+      rounded-full 
+      button
+      "
     >
       <img src={iconDice} alt="Icon dice" />
     </button>
